c8a923bde25458c9cdfc8f32c1117ecd4cddd8f0
avanti575 <64327729+avanti575@users.noreply.github.com>
2022-04-08 18:58:50 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -52,12 +52,12 @@ $('.menu-burger').click(function(){
       
  });
 // Меню слайдер
-const menuToggle = document.querySelector('#top__menu');
+// const menuToggle = document.querySelector('#top__menu');
 // const mobileNavContainer = document.querySelector('#mobile-nav')
-menuToggle.onclick = function(){
-    menuToggle.classList.toggle('top-menu-active');
-    // mobileNavContainer.classList.toggle('mobile-nav--active');
-}
+// menuToggle.onclick = function(){
+//     menuToggle.classList.toggle('top-menu-active');
+//     mobileNavContainer.classList.toggle('mobile-nav--active');
+// }
 
 
  // Кнопка наверх
